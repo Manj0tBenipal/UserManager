d8e2363f7c059ea9e44434c3774eee761d6d6fc3
Manj0tBenipal <manjotbenipal00001@gmail.com>
2023-09-10 11:20:12 -0400

    Renamed the main class file

--- src/main/java/ca/sheridancollege/sin13014/finalexam_manjot_singh/UserManagerApplication.java
+++ src/main/java/ca/sheridancollege/sin13014/finalexam_manjot_singh/UserManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalExamManjotSinghApplication {
+public class UserManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinalExamManjotSinghApplication.class, args);
+		SpringApplication.run(UserManagerApplication.class, args);
 	}
 
 }
